073354aa591517e2b923b68c51a20992ebdeeda0
VVayve <66766310+VVayve@users.noreply.github.com>
2022-03-20 21:22:08 -0400

    Add files via upload

---
+++ commands/music/play.js
@@ -0,0 +1,36 @@
+const { QueryType } = require('discord-player');
+
+module.exports = {
+    name: 'play',
+    aliases: ['p'],
+    utilisation: '{prefix}play [song name/URL]',
+    voiceChannel: true,
+
+    async execute(client, message, args) {
+if (!args[0]) return message.channel.send(`${message.author}, Please put the name or the song link that you would like to play after using "Synplay"!`);
+
+        const res = await client.player.search(args.join(' '), {
+            requestedBy: message.member,
+            searchEngine: QueryType.AUTO
+        });
+
+        if (!res || !res.tracks.length) return message.channel.send(`${message.author}, Oh no! Your song wasn't found in our server cheif, hmm. 🤔`);
+
+        const queue = await client.player.createQueue(message.guild, {
+            metadata: message.channel
+        });
+
+        try {
+            if (!queue.connection) await queue.connect(message.member.voice.channel);
+        } catch {
+            await client.player.deleteQueue(message.guild.id);
+            return message.channel.send(`${message.author}, I can't join the channel you are in 🤔`);
+        }
+
+        await message.channel.send(`Your ${res.playlist ? 'Your Playlist' : 'Your Track'} Playing! 🎧`);
+
+        res.playlist ? queue.addTracks(res.tracks) : queue.addTrack(res.tracks[0]);
+
+        if (!queue.playing) await queue.play();
+    },
+};
